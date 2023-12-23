05e60a32d7a539f9c6cbc8b912fa66008d021990
otavlna <otavlna@protonmail.ch>
2023-12-23 18:16:16 +0100

    feat: better auth forms

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,16 +4,27 @@ import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <div className="w-screen h-screen items-center flex justify-center">
-      <form className="flex flex-col items-center gap-5 w-[24rem] h-[16rem] border-blue-400 border-2 rounded-lg">
+      <form
+        action="http://localhost:8000/login/password"
+        method="post"
+        className="flex flex-col items-center gap-5 w-[24rem] h-[16rem] border-blue-400 border-2 rounded-lg"
+      >
         <h1 className="p-2 text-xl font-bold">Login</h1>
         <input
           type="text"
-          placeholder="Email"
+          name="username"
+          placeholder="Username"
+          autoComplete="username"
+          autoFocus
+          required
           className="bg-blue-200 text-center rounded-md"
         />
         <input
           type="password"
+          name="password"
           placeholder="Password"
+          autoComplete="current-password"
+          required
           className="bg-blue-200 text-center rounded-md"
         />
         <button type="submit" className="bg-blue-600 hover:bg-blue-500">
